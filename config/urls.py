aef8d8e7f24797969b33ebba4b4e16929f0cbf96
kiyoshion <kiyoshion@gmail.com>
2021-05-21 04:06:38 +0900

    Add templates dir and index.html

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView, TemplateView
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('accounts/', include('accounts.urls')),
+    path('shop/', include('shop.urls')),
 ]
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     import debug_toolbar
+
+#     urlpatterns += [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ]
