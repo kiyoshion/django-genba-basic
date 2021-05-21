e753fb4ff01918f03366dda1b70f1da7cc4f0132
kiyoshion <kiyoshion@gmail.com>
2021-05-21 17:22:33 +0900

    Update shop

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from django.views.generic import RedirectView
-# from . import views
+from . import views
 
 app_name = 'shop'
 urlpatterns = [
-  path('', RedirectView.as_view(url='/'), name='index'),
-  path('<int:book_id>/', RedirectView.as_view(url='/'), name='detail'),
-  path('checkout/', RedirectView.as_view(url='/'), name='checkout'),
+  path('', views.index, name='index'),
+  path('<int:book_id>/', views.detail, name='detail'),
+  path('checkout/', views.checkout, name='checkout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
 
-# Create your views here.
+from .models import Book
+
+class IndexView(LoginRequiredMixin, View):
+  def get(self, request, *args, **kwargs):
+    queryset = Book.objects.select_related('publisher').prefetch_related('authors').order_by('publish_date')
+    keyword = request.GET.get('keyword')
+    if keyword:
+      queryset = queryset.filter(
+        Q(title__icontains=keyword)
+      )
+    context = {
+      'keyword': keyword,
+      'book_list': queryset,
+    }
+    return render(request, 'shop/book_list.html', context)
+
+index = IndexView.as_view()
+
+class DetailView(LoginRequiredMixin, View):
+  def get(self, request, *args, **kwargs):
+    book = Book.objects.get(pk=book_id)
+    context = {
+      'book': book,
+    }
+    return render(request, 'shop/book_detail.html', context)
+
+detail = DetailView.as_view()
+
+class CheckoutView(LoginRequiredMixin, View):
+  def post(self, request, *args, **kwargs):
+    import time
+    start_time = time.time()
+
+    item_id = request.POST['item_id']
+    book = get_object_or_404(Book, pk=item_id)
+
+    return render(request, 'shop/complete.html')
+
+checkout = CheckoutView.as_view()
